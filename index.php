6fdff4761d2b290a7eefdf6da99f4ab540ac04d8
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-23 15:43:48 +0100

    add: aggiunto tabella

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docuudhfdihgidhgint</title>
+</head>
+<body>
+    
+<h1>ciaosssmdji</h1>
+
+</body>
+</html>
